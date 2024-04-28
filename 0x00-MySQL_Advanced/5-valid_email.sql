2c66d3d641d8ae963be1c4493b290597ee130b0b
Karenmiano <mianokaren1798@gmail.com>
2024-04-28 22:28:22 +0300

    trigger on change on specific column

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,11 +5,7 @@ CREATE TRIGGER changed_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-        IF OLD.valid_email = 0 THEN
-            SET NEW.valid_email = 1;
-        ELSE
-            SET NEW.valid_email = 0;
-        END IF;
-    END IF;
+        SET NEW.valid_email = 0;
+     END IF;   
 END$$
 DELIMITER ;
