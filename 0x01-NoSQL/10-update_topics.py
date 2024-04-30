0c1b7b56ee3f8d3e39207cae3d4e5b1e039361b6
Karenmiano <mianokaren1798@gmail.com>
2024-04-30 15:06:31 +0300

    update documents in python

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines the function update_topics"""
+
+def update_topics(mongo_collection, name, topics):
+    """updates all topics of a school document based on the name"""
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
