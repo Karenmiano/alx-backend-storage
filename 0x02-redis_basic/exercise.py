c8bb12fbd0780c7a7e5ea2b5e7d8c82479b5dc6f
Karenmiano <mianokaren1798@gmail.com>
2024-05-01 19:24:09 +0300

    writing to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,11 +5,12 @@ Defines the class Cache
 import redis
 import uuid
 
+
 class Cache:
     """Declares a Cache class"""
     def __init__(self):
         """Creates a connection to redis and flushes the db"""
-        self._redis: redis.Redis = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: str | bytes | int | float) -> str:
