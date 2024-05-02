ab92e747fcd21dc724084c5dd7ba92f6bb38ebc3
Karenmiano <mianokaren1798@gmail.com>
2024-05-02 05:36:04 +0300

    storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,7 +25,7 @@ def call_history(method: Callable) -> Callable:
     @wraps(method)
     def history(self, *args, **kwargs):
         """Stores history in redis"""
-        self._redis.rpush(f'{method.__qualname__}:inputs', str(*args))
+        self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
         output = method(self, *args, **kwargs)
         self._redis.rpush(f'{method.__qualname__}:outputs', output)
         return output
